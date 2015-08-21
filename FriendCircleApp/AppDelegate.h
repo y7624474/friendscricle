7f18bb5938779600568504c1ac7908493ef4f537
TWer <twer@YXL.local>
2015-08-21 09:40:49 +0800

    Initial Commit

---
+++ FriendCircleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FriendCircleApp
+//
+//  Created by TWer  on 8/21/15.
+//  Copyright (c) 2015 TWer . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FriendCircleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FriendCircleApp
+//
+//  Created by TWer  on 8/21/15.
+//  Copyright (c) 2015 TWer . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
